72218280ae54de6ef8d0ef862a5ace76bd157f11
Pasan Premaratne <pasan@premaratne.me>
2018-02-23 10:48:00 -0500

    Clean up createPages

--- gatsby-node.js
+++ gatsby-node.js
@@ -41,57 +41,6 @@ const createTagPages = (createPage, edges) => {
     });
 };
 
-// exports.createPages = ({ boundActionCreators, graphql }) => {
-//   const { createPage } = boundActionCreators;
-
-//   const blogPostTemplate = path.resolve(`src/templates/blog-post.js`);
-//   return graphql(`{
-//     allMarkdownRemark(
-//       sort: { order: DESC, fields: [frontmatter___date] }
-//       limit: 1000
-//     ) {
-//       edges {
-//         node {
-//           excerpt(pruneLength: 250)
-//           html
-//           id
-//           timeToRead
-//           frontmatter {
-//             date
-//             path
-//             tags
-//             title
-//           }
-//         }
-//       }
-//     }
-//   }`)
-//   .then(result => {
-//     if (result.errors) {
-//       return Promise.reject(result.errors)
-//     }
-
-//     const posts = result.data.allMarkdownRemark.edges;
-
-//     createTagPages(createPage, posts);
-//     // Create pages for each markdown file.
-//     posts.forEach(({ node }, index) => {
-//       const prev = index === 0 ? false : posts[index - 1].node;
-//       const next = index === posts.length - 1 ? false : posts[index + 1].node;
-//       createPage({
-//         path: node.frontmatter.path,
-//         component: blogPostTemplate,
-//         context: {
-//           prev,
-//           next
-//         }
-//       });
-//     });
-
-//     return posts;
-//   })
-// };
-
 exports.createPages = ({ boundActionCreators, graphql }) => {
   const { createPage } = boundActionCreators;
 
